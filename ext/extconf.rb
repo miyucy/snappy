7503baaf63dfc4cf86a8498eb7418667a36969b5
miyucy <miyucy@gmail.com>
2011-03-29 20:57:12 +0900

    change build script
    when snappy.so is not found, installing from source.
    proposed from pdlug, thx!

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,52 @@
 require 'mkmf'
+require 'fileutils'
 
-have_library('snappy')
-have_library('stdc++')
-create_makefile('snappy')
+unless have_library 'snappy'
+  have_header 'sys/mman.h'
+
+  have_header 'stdint.h'
+
+  have_header 'stddef.h'
+
+  if try_run 'int main(int argc, char** argv){ return __builtin_expect(1, 1) ? 0 : 1; }'
+    $defs << '-DHAVE_BUILTIN_EXPECT'
+  end
+
+  if try_run 'int main(int argc, char** argv){ return (__builtin_ctzll(0x100000000LL) == 32) ? 0 : 1; }'
+    $defs << '-DHAVE_BUILTIN_CTZ'
+  end
+
+  dst = File.expand_path Dir.pwd
+
+  ver = "1.0.1"
+  src = "snappy-#{ver}"
+  FileUtils.rm_rf src
+
+  system "curl -s http://snappy.googlecode.com/files/#{src}.tar.gz | tar xz"
+
+  %w(
+snappy-internal.h
+snappy-sinksource.cc
+snappy-sinksource.h
+snappy-stubs-internal.cc
+snappy-stubs-internal.h
+snappy.cc
+snappy.h
+  ).each do |file|
+    FileUtils.copy File.join(src, file), File.join(dst, file)
+  end
+
+  hdr = File.open(File.join(src, 'snappy-stubs-public.h.in'), 'rb'){ |f| f.read }
+  {
+    %r'#if @ac_cv_have_stdint_h@' => '#ifdef HAVE_STDINT_H',
+    %r'#if @ac_cv_have_stddef_h@' => '#ifdef HAVE_STDDEF_H',
+    %r'@SNAPPY_MAJOR@'            => '1',
+    %r'@SNAPPY_MINOR@'            => '0',
+    %r'@SNAPPY_PATCHLEVEL@'       => '1',
+  }.each { |ptn, str| hdr.gsub! ptn, str }
+  File.open(File.join(dst, 'snappy-stubs-public.h'), 'wb'){ |f| f.write hdr }
+
+end
+
+have_library 'stdc++'
+create_makefile 'snappy'
