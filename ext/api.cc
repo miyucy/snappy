7e52e757509db762f5aac565186534f6d2cfba11
Technorama Ltd <oss-ruby@technorama.net>
2011-10-01 11:29:28 -0400

    Use StringValue instead of to_s Take optional destination string argument like IO classes (performance)

--- ext/api.cc
+++ ext/api.cc
@@ -2,44 +2,58 @@
 #include "snappy.h"
 
 static VALUE rb_mSnappy;
-static ID    i_to_s;
 
 static VALUE
-snappy_deflate(VALUE self, VALUE source)
+snappy_deflate(int argc, VALUE *argv, VALUE self)
 {
+    VALUE src, dst;
     snappy::string output;
     size_t  output_length;
 
-    source = rb_funcall(source, i_to_s, 0);
-    output_length = snappy::Compress(RSTRING_PTR(source), RSTRING_LEN(source), &output);
+    rb_scan_args(argc, argv, "11", &src, &dst);
+    src = StringValue(src);
 
-    return rb_str_new(output.data(), output_length);
+    output_length = snappy::Compress(RSTRING_PTR(src), RSTRING_LEN(src), &output);
+    if (NIL_P(dst)) {
+        return rb_str_new(output.data(), output_length);
+    } else {
+    	StringValue(dst);
+    	rb_str_resize(dst, output_length);
+    	memcpy(RSTRING_PTR(dst), output.data(), output_length);
+    }
 }
 
 static VALUE
-snappy_inflate(VALUE self, VALUE source)
+snappy_inflate(int argc, VALUE *argv, VALUE self)
 {
+    VALUE src, dst;
     snappy::string output;
 
-    source = rb_funcall(source, i_to_s, 0);
-    if (!snappy::IsValidCompressedBuffer(RSTRING_PTR(source), RSTRING_LEN(source)) ||
-        !snappy::Uncompress(RSTRING_PTR(source), RSTRING_LEN(source), &output)) {
+    rb_scan_args(argc, argv, "11", &src, &dst);
+    src = StringValue(src);
+
+    if (!snappy::IsValidCompressedBuffer(RSTRING_PTR(src), RSTRING_LEN(src)) ||
+        !snappy::Uncompress(RSTRING_PTR(src), RSTRING_LEN(src), &output)) {
         rb_raise(rb_eRuntimeError, "Couldn't inflate");
     }
 
-    return rb_str_new(output.data(), output.size());
+    if (NIL_P(dst)) {
+        return rb_str_new(output.data(), output.size());
+    } else {
+    	StringValue(dst);
+    	rb_str_resize(dst, output.size());
+    	memcpy(RSTRING_PTR(dst), output.data(), output.size());
+    }
 }
 
 extern "C" {
 void Init_snappy()
 {
     rb_mSnappy = rb_define_module("Snappy");
-    rb_define_singleton_method(rb_mSnappy, "deflate", (VALUE (*)(...))snappy_deflate, 1);
-    rb_define_singleton_method(rb_mSnappy, "inflate", (VALUE (*)(...))snappy_inflate, 1);
+    rb_define_singleton_method(rb_mSnappy, "deflate", (VALUE (*)(...))snappy_deflate, -1);
+    rb_define_singleton_method(rb_mSnappy, "inflate", (VALUE (*)(...))snappy_inflate, -1);
 
-    rb_define_singleton_method(rb_mSnappy, "compress", (VALUE (*)(...))snappy_deflate, 1);
-    rb_define_singleton_method(rb_mSnappy, "uncompress", (VALUE (*)(...))snappy_inflate, 1);
-
-    i_to_s = rb_intern("to_s");
+    rb_define_singleton_method(rb_mSnappy, "compress", (VALUE (*)(...))snappy_deflate, -1);
+    rb_define_singleton_method(rb_mSnappy, "uncompress", (VALUE (*)(...))snappy_inflate, -1);
 }
 }
