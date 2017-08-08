98ff00abe0e1781ce36f848e78a0287a0985bcc6
miyucy <fistfvck@gmail.com>
2017-08-08 13:09:06 +0900

    remove force option

--- smoke.sh
+++ smoke.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 gem list | grep snappy && gem uninstall --force snappy
 bundle exec rake clean build
-gem install --force --local --no-ri --no-rdoc "$(ls pkg/snappy-*.gem)"
+gem install --no-ri --no-rdoc "$(ls pkg/snappy-*.gem)"
 cat <<EOF | ruby
 require 'snappy'
 abort if Snappy.inflate(Snappy.deflate(File.read('smoke.sh'))) != File.read('smoke.sh')
